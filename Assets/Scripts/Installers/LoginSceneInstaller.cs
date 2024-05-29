b9e21d3ac5649a9cfab1803090b6817984e449a1
Ekrem Taha Ünlü <ekremtahaunlu@users.noreply.github.com>
2024-05-29 15:28:14 +0300

    Grid structure was created with Extenject.

---
+++ Assets/Scripts/Installers/LoginSceneInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Installers
+{
+  public class LoginSceneInstaller : MonoInstaller<LoginSceneInstaller>
+  {
+    public override void InstallBindings()
+    {
+            
+    }
+  }
+}
