944b17f3479b730aae32c4bebc5d9c95ab109a8e
Ekrem Taha Ünlü <ekremtahaunlu@users.noreply.github.com>
2024-08-08 16:48:33 +0300

    a/b test provider

--- Assets/Scripts/Components/CameraSizeFitter.cs
+++ Assets/Scripts/Components/CameraSizeFitter.cs
@@ -5,26 +5,34 @@ using Zenject;
 
 namespace Components
 {
-  public class CameraSizeFitter : EventListenerMono
-  {
-    [Inject] private GridEvents GridEvents{get;set;}
-    [SerializeField] private Camera _camera;
-    [SerializeField] private Transform _transform;
-        
-    protected override void RegisterEvents()
-    {
-      GridEvents.GridLoaded += OnGridLoaded;
-    }
+	public class CameraSizeFitter : EventListenerMono
+	{
+		[Inject] private GridEvents GridEvents{get;set;}
+		[Inject] private InputEvents InputEvents{get;set;}
+		[SerializeField] private Camera _camera;
+		[SerializeField] private Transform _transform;
 
-    private void OnGridLoaded(Bounds gridBounds)
-    {
-      _transform.position = gridBounds.center + (Vector3.back * 10f);
-      _camera.orthographicSize = gridBounds.extents.x * (1f / _camera.aspect);
-    }
+		protected override void RegisterEvents()
+		{
+			GridEvents.GridLoaded += OnGridLoaded;
+			InputEvents.ZoomDelta += OnZoomDelta;
+		}
 
-    protected override void UnRegisterEvents()
-    {
-      GridEvents.GridLoaded -= OnGridLoaded;
-    }
-  }
+		private void OnZoomDelta(float arg0)
+		{
+			_camera.orthographicSize += arg0;
+		}
+
+		private void OnGridLoaded(Bounds gridBounds)
+		{
+			_transform.position = gridBounds.center + (Vector3.back * 10f);
+			_camera.orthographicSize = gridBounds.extents.x * (1f / _camera.aspect);
+		}
+
+		protected override void UnRegisterEvents()
+		{
+			GridEvents.GridLoaded -= OnGridLoaded;
+			InputEvents.ZoomDelta -= OnZoomDelta;
+		}
+	}
 }
--- Assets/Scripts/Components/InputListener.cs
+++ Assets/Scripts/Components/InputListener.cs
@@ -1,4 +1,5 @@
-﻿using Events;
+﻿﻿using Events;
+using Extensions.System;
 using Extensions.Unity;
 using Extensions.Unity.MonoHelper;
 using UnityEngine;
@@ -6,62 +7,97 @@ using Zenject;
 
 namespace Components
 {
-  public class InputListener : EventListenerMono
-  {
-    [Inject] private InputEvents InputEvents{get;set;}
-    //[Inject] private AudioManager AudioManager { get; set; }
-    [Inject] private Camera Camera{get;set;}
-    [Inject] private GridEvents GridEvents{get;set;}
-    private RoutineHelper _inputRoutine;
-    
-    private void Awake() {_inputRoutine = new RoutineHelper(this, null, InputUpdate);}
-
-    private void InputUpdate()
+    public class InputListener : EventListenerMono
     {
-      if(Input.GetMouseButtonDown(0))
-      {
-        Ray inputRay = Camera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit[] hits = Physics.RaycastAll(inputRay, 100f);
-        
-        //AudioManager.Play("TileClick");
+        private const float ZoomDeltaThreshold = 0.01f;
+        [Inject] private InputEvents InputEvents{get;set;}
+        [Inject] private Camera Camera{get;set;}
+        [Inject] private GridEvents GridEvents{get;set;}
+        private RoutineHelper _inputRoutine;
+        private float _lastDist;
+        private int _lastTouchCount;
 
-        Tile firstHitTile = null;
+        private void Awake() {_inputRoutine = new RoutineHelper(this, null, InputUpdate);}
 
-        foreach(RaycastHit hit in hits)
+        private void InputUpdate()
         {
-          if(hit.transform.TryGetComponent(out firstHitTile)) break;
+            if(Input.GetMouseButtonDown(0))
+            {
+                Ray inputRay = Camera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit[] hits = Physics.RaycastAll(inputRay, 100f);
+
+                Tile firstHitTile = null;
+
+                foreach(RaycastHit hit in hits)
+                {
+                    if(hit.transform.TryGetComponent(out firstHitTile)) break;
+                }
+
+                InputEvents.MouseDownGrid?.Invoke(firstHitTile, inputRay.origin + inputRay.direction);
+            }
+            else if(Input.GetMouseButtonUp(0))
+            {
+                Ray inputRay = Camera.ScreenPointToRay(Input.mousePosition);
+                
+                InputEvents.MouseUpGrid?.Invoke(inputRay.origin + inputRay.direction);
+            }
+
+            int touchCount = Input.touchCount;
+            
+            if(touchCount > 1)
+            {
+                Touch touch1 = Input.GetTouch(0);
+
+                Touch touch2 = Input.GetTouch(1);
+
+                float currDist = (touch1.position - touch2.position).magnitude;
+
+                if(_lastTouchCount < 2)
+                {
+                    _lastTouchCount = touchCount;
+                    
+                    _lastDist = currDist;
+                    return;
+                }
+
+                float distDelta = _lastDist - currDist;
+
+                if(distDelta.Abs() >= ZoomDeltaThreshold)
+                {
+                    Debug.LogWarning($"distDelta {distDelta}");
+                    InputEvents.ZoomDelta?.Invoke(distDelta);
+                }
+
+                _lastTouchCount = touchCount;
+                _lastDist = currDist;
+            }
+            else
+            {
+                _lastTouchCount = 0;
+                _lastDist = 0f;
+            }
         }
 
-        InputEvents.MouseDownGrid?.Invoke(firstHitTile, inputRay.origin + inputRay.direction);
-      }
-      else if(Input.GetMouseButtonUp(0))
-      {
-        Ray inputRay = Camera.ScreenPointToRay(Input.mousePosition);
-                
-        InputEvents.MouseUpGrid?.Invoke(inputRay.origin + inputRay.direction);
-      }
-    }
+        protected override void RegisterEvents()
+        {
+            GridEvents.InputStart += OnInputStart;
+            GridEvents.InputStop += OnInputStop;
+        }
 
-    protected override void RegisterEvents()
-    {
-      GridEvents.InputStart += OnInputStart;
-      GridEvents.InputStop += OnInputStop;
-    }
+        private void OnInputStop()
+        {
+            _inputRoutine.StopCoroutine();
+        }
 
-    private void OnInputStop()
-    {
-      _inputRoutine.StopCoroutine();
-    }
+        private void OnInputStart()
+        {
+            _inputRoutine.StartCoroutine();
+        }
 
-    private void OnInputStart()
-    {
-      _inputRoutine.StartCoroutine();
+        protected override void UnRegisterEvents()
+        {
+            GridEvents.InputStart -= OnInputStart;
+            GridEvents.InputStop -= OnInputStop;
+        }
     }
-
-    protected override void UnRegisterEvents()
-    {
-      GridEvents.InputStart -= OnInputStart;
-      GridEvents.InputStop -= OnInputStop;
-    }
-  }
 }
